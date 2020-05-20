3784d9932e42390ffef8b5094b668eaf24a409a5
Tanzila Hassan <63036082+tan99995@users.noreply.github.com>
2020-05-20 23:22:30 +1000

    Add files via upload

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'institution', 'password',
     ];
 
     /**
@@ -27,7 +27,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
+    
     /**
      * The attributes that should be cast to native types.
      *
