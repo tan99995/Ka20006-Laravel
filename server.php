4fb0c38038140d13d2fa714f7ba28546adb317a1
Menelik Gedion <menilik98@gmail.com>
2020-05-21 12:36:57 +1000

    as

--- server.php
+++ server.php
@@ -1,12 +1,12 @@
-<?php
 
+<!-- 
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
- */
-
+ */ -->
+ <?php 
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
@@ -18,4 +18,5 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
 
-require_once __DIR__.'/public/index.php';
+require_once __DIR__.'/public/index.php'; 
+
