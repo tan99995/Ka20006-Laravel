9033821fa46249197da458d624213410866aa096
Menelik Gedion <menilik98@gmail.com>
2020-05-25 16:39:46 +1000

    Calculation page workong

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -81,13 +81,8 @@
                         </div>
 
                         <div class="form-group row ">
-<<<<<<< Updated upstream
                           <div class="col-md-8 offset-md-5" >
-                                <button type="submit" class="btn btn-primary" style = "display:flex;align-item:center;">
-=======
-                          <div class="col-sm-offset-2 col-sm-10" >
                                 <button type="submit" class="btn btn-primary">
->>>>>>> Stashed changes
                                     {{ __('Register') }}
                                 </button>
                             </div>  
